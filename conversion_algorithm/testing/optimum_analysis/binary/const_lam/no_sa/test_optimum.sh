cf68243ab96daef5721e4ddcebaaf1699cc55b01
justusm00 <j.multhaup2000@gmail.com>
2023-04-24 10:12:18 +0200

    python3 routines

--- conversion_algorithm/testing/optimum_analysis/binary/const_lam/no_sa/test_optimum.sh
+++ conversion_algorithm/testing/optimum_analysis/binary/const_lam/no_sa/test_optimum.sh
@@ -2,21 +2,19 @@
 # @Author: Your name
 # @Date:   2023-02-09 11:41:03
 # @Last Modified by:   Your name
-# @Last Modified time: 2023-04-23 21:47:34
+# @Last Modified time: 2023-04-23 21:46:33
 
 #remove old files
 rm *.csv
-
-
 ##create configuration
 python3 ConfGen.py -i coord.xml
 wait
 python3 config_umbrella.py coord.h5 1
 wait
 #iterate over different seeds
-for j in {1..1000}
+for i in {1..1000}
 do
-    ./SOMA -c coord.h5 -t 1  > temp.txt
+    ./SOMA -c coord.h5 -t 1 > temp.txt
     grep 'MSE after flips at T=0:' temp.txt | sed 's/^.*: //' >> opt.csv
     rm temp.txt
     #wait one second to obtain new seed
---
+++ conversion_algorithm/testing/optimum_analysis/ternary/const_lam/sa/var_alpha/test_optimum.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# @Author: Your name
+# @Date:   2023-02-09 11:41:03
+# @Last Modified by:   Your name
+# @Last Modified time: 2023-04-20 13:57:46
+
+#remove old files
+rm *.csv
+
+#iterate over temperatures
+for i in {1..10}
+do
+    t=$(echo "scale=5; 0.8+$i/100" | bc -l)
+    echo "$t" >> alpha.csv
+    sed -i''  -e "s/<alpha>.*/<alpha>$t<\/alpha>/g" coord.xml
+    #modify coord.xml Tmax
+    
+    ##create configuration
+    python3 ConfGen.py -i coord.xml
+    wait
+    python3 config_umbrella.py coord.h5 1
+    wait
+    #iterate over different seeds
+    for j in {1..10}
+    do
+        ./SOMA -c coord.h5 -t 1 -r 0 > temp.txt
+        grep 'MSE after flips at T=0:' temp.txt | sed 's/^.*: //' >> opt.csv
+        rm temp.txt
+        #wait one second to obtain new seed
+        sleep 0.5
+    done
+done
